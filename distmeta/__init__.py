5a2072ba82dc09bbea4fc475c269b426a8e1345d
Michael Mulich <michael.mulich@gmail.com>
2011-01-13 16:53:19 -0500

    Started the makings of a repository object. Added the first testcase for the repository class. Added an example repository, that was made by hand for now.

--- distmeta/__init__.py
+++ distmeta/__init__.py
@@ -1,2 +1,49 @@
 # -*- coding: utf-8 -*-
+import os
+from distutils2.metadata import DistributionMetadata
+
+__all__ = ('MetadataRepository',)
+
+
+class MetadataRepository(object):
+    """A repository of Python distribution metadata stored in a directory
+    structure on the file system. The structure would organized by
+    distribution name then by release (or version)."""
+
+    def __init__(self, location):
+        self.location = location
+        if not os.path.exists(self.location):
+            raise Exception("Can't find the repository location at %s"
+                            % location)
+        #: Intialize the data in a dictionary of distribution names
+        #  (keys) with a list of versions (values).
+        self._repo = {}
+        self._init_repo()
+
+    def _init_repo(self):
+        """Initialize the data in self._repo from a filesystem structure."""
+        if not os.path.isdir(self.location):
+            raise Exception("Expected a distribution metadata structure at "
+                            "%s, but found a file instead." % self.location)
+        for dist in os.listdir(self.location):
+            dist_path = os.path.join(self.location, dist)
+            if dist.startswith('.') \
+               or os.path.isfile(dist_path) \
+               or os.path.islink(dist_path):
+                #: Nevermind, it's not a distribution...
+                continue
+            for version in os.listdir(dist_path):
+                if dist not in self._repo:
+                    self._repo[dist] = []
+                metadata_file = os.path.join(dist_path, version, 'METADATA')
+                if os.path.exists(metadata_file):
+                    self._repo[dist].append(version)
+                else:
+                    # XXX Need to warn the user that the metadata is missing!
+                    pass
+
+    def __repr__(self):
+        class_name = self.__class__.__name__ 
+        abs_location = os.path.abspath(self.location)
+        return '%s("%s")' % (class_name, abs_location)
 
--- distmeta/tests/test_repository.py
+++ distmeta/tests/test_repository.py
@@ -2,8 +2,10 @@
 import os
 from distmeta.tests import unittest
 
+HERE = os.path.abspath(os.path.dirname(__file__))
 
-class TestCase(unittest.TestCase):
+
+class TestMetadataRepository(unittest.TestCase):
     """First test to flesh out the metadata directory storage structure.
     This will be the foundation for example data to be used throughout the
     test environment."""
@@ -14,4 +16,8 @@ class TestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-
+    def test_load(self):
+        temp_repo = os.path.join(HERE, 'metadata_repo')
+        from distmeta import MetadataRepository
+        repo = MetadataRepository(temp_repo)
+        self.assertIn('solarcal', repo)
