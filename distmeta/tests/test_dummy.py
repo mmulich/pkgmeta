ac5a19f0a66cd2312784fa94d9e9859b008fedb7
Michael Mulich <michael.mulich@gmail.com>
2011-01-13 15:10:26 -0500

    Initialing the testing structure.

---
+++ distmeta/tests/test_dummy.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import os
+from distmeta.tests import unittest
+
+
+class TestCase(unittest.TestCase):
+    """First test to flesh out the metadata directory storage structure.
+    This will be the foundation for example data to be used throughout the
+    test environment."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
