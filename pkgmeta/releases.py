75cef83aaceb19c1176148e6feeadca0a88642d2
Michael Mulich <michael.mulich@gmail.com>
2011-05-30 10:10:55 -0400

    Reworked the release set to a mapping, but inside it is still a list of releases.

--- pkgmeta/releases.py
+++ pkgmeta/releases.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
-from pkgmeta.metadata import DistributionMetadata
+from collections import Mapping
+from pkgmeta.metadata import Metadata
 
 __all__ = ('ReleaseSet',)
 
 
-class ReleaseSet(list):
+class ReleaseSet(Mapping):
     """A set of releases for a distribution ordered by version number.
 
     Initialized by an optional list of dist-info directories.
     """
 
     def __init__(self, iterable=[]):
-        super(ReleaseSet, self).__init__(iterable)
+        self.releases = list(iterable)
         self._reorder()
         # FIXME: Find a better way to determine the stable release.
         self._stable_release = len(self) - 1
@@ -20,28 +21,37 @@ class ReleaseSet(list):
     def _reorder(self):
         """Reorder the releases by version number from lowest (0) to highest.
         """
-        self.sort()
+        self.releases.sort()
 
     @classmethod
-    def from_path(cls, path):
+    def from_directory(cls, path):
         """Initialize the set from a filesystem path."""
         releases = []
         for release in os.listdir(path):
             metadata_file = os.path.join(path, release, 'METADATA')
-            metadata = DistributionMetadata(path=metadata_file)
+            metadata = Metadata(path=metadata_file)
             releases.append(metadata)
         return cls(releases)
 
     def __hash__(self):
         release_hash = 0
-        for release in self:
+        for release in self.releases:
             release_hash += hash(release)
         return len(self) + release_hash
 
     @property
     def name(self):
         try:
-            name = self[self._stable_release].get('Name')
+            name = self.releases[self._stable_release].get('Name')
         except IndexError:
             name = 'UNKNOWN'
         return name
+
+    def __len__(self):
+        return len(self.releases)
+
+    def __iter__(self):
+        return self.releases[self._stable_release].__iter__()
+
+    def __getitem__(self, key):
+        return self.releases[self._stable_release][key]
