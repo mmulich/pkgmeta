fc04a968195ec88685c3d5666d8771978e79f36b
Michael Mulich <michael.mulich@gmail.com>
2011-05-30 10:13:04 -0400

    Rewrote the Repository class to subclass Mapping.

--- pkgmeta/repository.py
+++ pkgmeta/repository.py
@@ -1,87 +1,92 @@
 # -*- coding: utf-8 -*-
 import os
-from UserDict import DictMixin
-from pkgmeta.metadata import DistributionMetadata
+from collections import Mapping
+from pkgmeta.metadata import Metadata
 from pkgmeta.releases import ReleaseSet
 
-__all__ = ('MetadataRepository',)
+__all__ = ('Repository',)
 
 
 class RepositoryIsNotMutable(Exception):
     """Repositories are not mutable once they have been read in."""
 
 
-class MetadataRepository(object, DictMixin):
-    """A repository of Python distribution metadata stored in a directory
-    structure on the file system. The structure would organized by
-    distribution name then by release (or version)."""
+class Repository(Mapping):
+    """A repository of Python distribution metadata. The structure is
+    organized by package name then by release (version)."""
 
-    def __init__(self, data=[]):
-        #: Intialize the data in a dictionary of distribution names
-        #  (keys) with a list of versions (values).
-        self._data = data
+    def __init__(self, _data={}, **kwargs):
+        #: Initialize the data in a dictionary of package names (keys)
+        #  with a list of versions (values).
+        if not isinstance(_data, dict):
+            raise TypeError("First argument should be a dictionary object")
+        self._data = _data
+        self._data.update(kwargs)
 
     @classmethod
-    def from_path(cls, path):
-        """Initialize the data in self._repo from a filesystem structure."""
+    def from_directory(cls, path):
+        """Initialize the data from a filesystem directory structure."""
+        from_init_name = 'from_directory'
+        path = os.path.abspath(path)
+
         if not os.path.exists(path):
             raise Exception("Can't find the repository location at %s"
                             % location)
         elif not os.path.isdir(path):
             raise Exception("Expected a distribution metadata structure at "
                             "%s, but found a file instead." % self.location)
+
         structure = os.walk(path)
-        root, dist_dirs = structure.next()[:2]
-        data = [ReleaseSet.from_path(os.path.join(root, dist))
-                for dist in dist_dirs]
+        root, dist_dirs = next(structure)[:2]
+        data = {dist:ReleaseSet.from_directory(os.path.join(root, dist))
+                for dist in dist_dirs}
         inst = cls(data)
         inst.path = path
+        inst.__from__ = from_init_name
         return inst
 
     def __repr__(self):
         cls_name = self.__class__.__name__
-        if hasattr(self, 'path'):
-            abs_location = os.path.abspath(self.path)
-            return '%s.from_path("%s")' % (cls_name, abs_location)
-        releases_repr = ', '.join([repr(x) for x in self._data])
-        return '<%s of %s>' % (cls_name, releases_repr)
-
-    # Container emulation:
-    # http://docs.python.org/reference/datamodel.html#emulating-container-types
-    def __setitem__(self, key, value):
-        if key in self:
-            raise NotImplementedError("Not sure what to do yet.")
-        if isinstance(value, ReleaseSet):
-            raise NotImplementedError("todo...")
-        elif isinstance(value, DistributionMetadata):
-            raise NotImplementedError("todo...")
+        if hasattr(self, '__from__'):
+            cls_init = '.'.join([cls_name, self.__from__])
         else:
-            raise TypeError("Unknown value type: %s" % type(value))
+            cls_init = cls_name
+        if hasattr(self, 'path'):
+            representation = '%s("%s")' % (cls_init, self.path)
+        else:
+            releases_repr = ', '.join([repr(x) for x in self._data])
+            representation = '<%s of %s>' % (cls_name, releases_repr)
+        return representation
+
+    # ############################### #
+    #   Abstract method definitions   #
+    # ############################### #
 
     def __getitem__(self, key):
-        release_set = None
-        for rs in self._data:
-            if rs.name == key:
-                release_set = rs
-                break
-        if not release_set:
-            raise KeyError("Release set for %s could not be found" % key)
-        return release_set
+        return self._data[key]
 
-    def __delitem__(self, key):
-        raise RepositoryIsNotMutable("This repository is read-only.")
+    def __iter__(self):
+        return self._data.__iter__()
 
-    def keys(self):
-        return [rs.name for rs in self._data]
+    def __len__(self):
+        return len(self._data)
 
-    # Public API
+    # ############## #
+    #   Public API   #
+    # ############## #
+
     def search(self, search_callable, property_names=['name']):
-        if not callable(search_callable):
+        if not hasattr(search_callable, '__call__'):
+            # Must be a callable that returns a boolean
             raise TypeError()
         if not hasattr(property_names, '__iter__'):
+            # Must be a sequence
             property_names = [property_names]
         search_results = []
         for prop in property_names:
-            search_results.extend([rs for rs in self._data
-                                   if search_callable(getattr(rs, prop))])
+            ##search_results.extend([rs for rs in self._data
+            ##                       if search_callable(getattr(rs, prop))])
+            for release_set in self._data.values():
+                if search_callable(getattr(release_set, prop)):
+                    search_results.append(release_set)
         return set(search_results)
