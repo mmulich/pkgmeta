8acd0e62df55ac22d2fa4be4756ee6d5828d2762
Michael Mulich <michael.mulich@gmail.com>
2011-11-13 12:04:13 -0500

    method renamed to keep somewhat consistent

--- pkgmeta/tests/test_metadata.py
+++ pkgmeta/tests/test_metadata.py
@@ -5,7 +5,7 @@ from pkgmeta.tests import unittest
 class TestMetadataComparisons(unittest.TestCase):
     """Test for metadata version comparison functionality."""
 
-    def makeOne(self, **kwargs):
+    def make_one(self, **kwargs):
         from pkgmeta.metadata import Metadata
         return Metadata(**kwargs)
 
@@ -13,21 +13,21 @@ class TestMetadataComparisons(unittest.TestCase):
         #: Set up the metadata objects.
 
         mapping = {'name': 'common', 'version': '1.0'}
-        meta_one = self.makeOne(mapping=mapping)
-        meta_two = self.makeOne(mapping=mapping)
+        meta_one = self.make_one(mapping=mapping)
+        meta_two = self.make_one(mapping=mapping)
         self.assertEqual(meta_one, meta_two)
         #: Change the name to ensure that differnt dist
         #  with the same version aren't equal.
         mapping['name'] = 'notcommon'
-        meta_two = self.makeOne(mapping=mapping)
+        meta_two = self.make_one(mapping=mapping)
         self.assertNotEqual(meta_one, meta_two)
 
     def test_less_or_lessequal(self):
         #: Set up the metadata objects.
-        meta_one = self.makeOne(mapping={'name': 'common',
-                                         'version': '1.0'})
-        meta_two = self.makeOne(mapping={'name': 'common',
-                                         'version': '2.0'})
+        meta_one = self.make_one(mapping={'name': 'common',
+                                          'version': '1.0'})
+        meta_two = self.make_one(mapping={'name': 'common',
+                                          'version': '2.0'})
         self.assertLess(meta_one, meta_two)
         #: Change the name to ensure that differnt dist
         #  with the same version aren't equal.
@@ -35,10 +35,10 @@ class TestMetadataComparisons(unittest.TestCase):
 
     def test_greater_or_greaterequal(self):
         #: Set up the metadata objects.
-        meta_one = self.makeOne(mapping={'name': 'common',
-                                         'version': '1.0.1'})
-        meta_two = self.makeOne(mapping={'name': 'common',
-                                         'version': '1.0a1'})
+        meta_one = self.make_one(mapping={'name': 'common',
+                                          'version': '1.0.1'})
+        meta_two = self.make_one(mapping={'name': 'common',
+                                          'version': '1.0a1'})
         self.assertGreater(meta_one, meta_two)
         #: Change the name to ensure that differnt dist
         #  with the same version aren't equal.
