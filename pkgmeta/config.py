03025816f600385510c878170c2a19049b5adf07
Michael Mulich <michael.mulich@gmail.com>
2011-11-13 01:27:55 -0500

    Simplify the config to something that isn't file system based. We can add a file system based config later.

--- pkgmeta/config.py
+++ pkgmeta/config.py
@@ -1,109 +1,49 @@
 # -*- coding: utf-8 -*-
 import os
 import sysconfig
-from configparser import ConfigParser
-from pkgmeta.exceptions import ConfigNotFound, ConfigReadError
 
-__all__ = ('PkgMetaConfig', 'RepositoryConfig',
-           'PKGMETA_CFGS', 'PKGMETA_CFG_LOCATIONS',)
+__all__ = ('PkgMetaConfig', 'RepositoryConfig',)
 
 # FIXME: Should be using packaging.resources.get_file{_path}, but these are
 #        currently not working in the tests because of missing logic in
 #        packaging.command.test.
 
-_sysconfig_paths = sysconfig.get_paths()
-PKG_HOME = os.path.dirname(__file__)
-BASE_CONFIG = os.path.join(PKG_HOME, 'base_config')
-PKGMETA_CFG_LOCATIONS = os.path.join(BASE_CONFIG, 'pkgmeta.cfg-locations')
 
-LOCAL_CONFIG_VARS = {
-    'pkgmeta_home': PKG_HOME,
-    }
-
-
-def _substitute_config_vars(path, local_vars=LOCAL_CONFIG_VARS):
-    """Like sysconfig._subst_vars except with pkgmeta custom variables included.
-    """
-    config_vars = _sysconfig_paths.copy()
-    sysconfig._extend_dict(config_vars, local_vars)
-    path = sysconfig._subst_vars(path, config_vars)
-    return path
-
-
-def _retrieve_config_list(list_file):
-    """Retrieve a list of configuration files that actually exist on the system
-    from paths that use sysconfig variables."""
-    results = []
-    with open(list_file, 'r') as f:
-    # Note: Tuple comprehension won't work here, because it makes a generator.
-        for path in f.readlines():
-            path = path.strip()
-            if not path or path.startswith('#'):
-                continue
-            path = _substitute_config_vars(path)
-            if os.path.exists(path):
-                results.append(path)
-    return results
-
-# There are multiple locations where pkgmeta.cfg configs can be found.
-PKGMETA_CFGS = tuple(_retrieve_config_list(PKGMETA_CFG_LOCATIONS))
-
-
-class PkgMetaConfig(ConfigParser):
-    """Configuration parser for pkgmeta.cfg initialization.
-
-    This fills in any missing repository configuration and sets a
-    bunch of defaults.
+class PkgMetaConfig(object):
+    """Configuration
     """
 
-    def __init__(self, cfg=None):
-        super(PkgMetaConfig, self).__init__(allow_no_value=True)
-        if cfg is None:
-            cfg = PKGMETA_CFGS[0]
-        self.read(cfg)
-        if not hasattr(self, 'default'):
-            self.default = self.sections()[0]
-        self._file = cfg
-
-    def read(self, filenames):
-        """Reads configuration from a sequence."""
-        if hasattr(self, '_file'):
-            # This prevents anyone from loading in another config.
-            raise RuntimeError("Only one config can be read in.")
-        # Ensure only one file can be read in.
-        if isinstance(filenames, (list, tuple)):
-            filename = filenames[0]
-        else:
-            filename = filenames
-        super(PkgMetaConfig, self).read(filename)
-        # Verify the global section is in tact
-        if not self.has_section('global'):
-            raise ConfigReadError("[global] section missing.")
-        elif not self.has_option('global', 'root'):
-            raise ConfigReadError("[global] section missing a root value")
-        # Read in global variables and remove the global variables section
-        for name, value in self.items('global'):
-            new_value = _substitute_config_vars(value)
-            setattr(self, name, new_value)
-        self.remove_section('global')
+    def __init__(self, repositories, default=None):
+        if not hasattr(repositories, '__iter__'):
+            repositories = [repositories]
+        self.repositories = repositories
+        for repo in self.repositories:
+            if not isinstance(repo, RepositoryConfig):
+                raise TypeError("%s is not a RepsitoryConfig" % repr(repo))
+        if default is None:
+            self.default = repositories[0].name
 
     def list_repositories(self):
         """List available repositories."""
-        return self.sections()
+        return iter(self.repositories)
 
     def get_repository_config(self, name=None):
-        """Get a RepositoryConfig by name. If name is not given, the default or first found
-        repository will be returned."""
+        """Get a RepositoryConfig by name. If name is not given,
+        the default repository will be returned."""
         if name is None:
             name = self.default
-        location = os.path.join(self.root, name)
-        sources = [source for source in self.get(name, 'sources').split() if source] 
-        return RepositoryConfig(name, location, sources)
+        try:
+            repo_config = [r for r in self.repositories if r.name == name][0]
+        except IndexError:
+            raise LookupError("Could not find '%s'" % name)
+        return repo_config
 
 
 class RepositoryConfig:
+    """A repository configuration"""
 
-    def __init__(self, name, location, sources=[]):
+    def __init__(self, name, sources=None):
         self.name = name
-        self.location = location
         self.sources = sources
+        if self.sources is None:
+            self.sources = []
--- pkgmeta/tests/test_config.py
+++ pkgmeta/tests/test_config.py
@@ -5,99 +5,62 @@ import tempfile
 from pkgmeta.tests import unittest
 
 
-TEST_PKGMETA_CFG = """\
-[global]
-root = {appdata.persistent}
-cache = {appdata.disposable}
+# TEST_PKGMETA_CFG = """\
+# [global]
+# root = {appdata.persistent}
+# cache = {appdata.disposable}
 
-[test1-repo]
-sources =
-    %(source_one)s
+# [test1-repo]
+# sources =
+#     %(source_one)s
 
-[test2-repo]
-sources =
-    %(source_two)s
-    %(source_one)s
-"""
+# [test2-repo]
+# sources =
+#     %(source_two)s
+#     %(source_one)s
+# """
 
-SOURCES = {'source_one': 'file:///', 'source_two': 'file:///'}
+# SOURCES = {'source_one': 'file:///', 'source_two': 'file:///'}
 
 
 class PkgMetaConfigTestCase(unittest.TestCase):
 
-    def setUp(self):
-        # Testing config file
-        self.pkgmeta_cfg = tempfile.mkstemp('.cfg', 'pkgmeta-')[1]
+    def make_repo_config(self, *args, **kwargs):
+        """Make a RepositoryConfig object."""
+        from pkgmeta.config import RepositoryConfig
+        return RepositoryConfig(*args, **kwargs)
 
-    def tearDown(self):
-        os.remove(self.pkgmeta_cfg)
-
-    def _init_config(self, sources=SOURCES):
-        with open(self.pkgmeta_cfg, 'w') as f:
-            f.write(TEST_PKGMETA_CFG % sources)        
-
-    def _make_one(self, cfg=None):
+    def make_one(self, *args, **kwargs):
         from pkgmeta.config import PkgMetaConfig
-        if cfg is not None:
-            return PkgMetaConfig(cfg)
-        return PkgMetaConfig()
-
-    def test_global_config_section_missing(self):
-        # No global config section found in the config
-        from pkgmeta.exceptions import ConfigReadError
-        with open(self.pkgmeta_cfg, 'w') as f:
-            f.write('[no-global-but-not-empty]')
-        with self.assertRaises(ConfigReadError) as err:
-            self._make_one(self.pkgmeta_cfg)
-        self.assertEqual(err.exception.message, "[global] section missing.")
-
-    def test_partial_necessary_configuration(self):
-        # Partial config without the root option
-        from pkgmeta.exceptions import ConfigReadError
-        with open(self.pkgmeta_cfg, 'w') as f:
-            f.write("[global]")
-        with self.assertRaises(ConfigReadError) as err:
-            self._make_one(self.pkgmeta_cfg)
-        self.assertEqual(err.exception.message,
-                         "[global] section missing a root value")
-
-    def test_global_config_vars_load(self):
-        self._init_config()
-        # Testing config
-        from pkgmeta import config
-        # Two in one test, since we aren't giving the configuratin a pkgmeta.cfg path.
-        config.PKGMETA_CFGS = [self.pkgmeta_cfg]
-        # Without a cfg path argument, the object will instantiate with the first value in
-        # PKGMETA_CFGS.
-        config = self._make_one()
-        self.assertEqual(config._file, self.pkgmeta_cfg)
-        self.assertEqual(config.root, sysconfig.get_paths()['appdata.persistent'])
-        self.assertEqual(config.cache, sysconfig.get_paths()['appdata.disposable'])
+        return PkgMetaConfig(*args, **kwargs)
 
     def test_list_repositories(self):
-        self._init_config()
         # List configuration
-        config = self._make_one(self.pkgmeta_cfg)
-        self.assertEqual(config.list_repositories(), ['test1-repo', 'test2-repo'])
+        repo_names = ['test1-repo', 'test2-repo']
+        repos = [self.make_repo_config(n) for n in repo_names]
+        config = self.make_one(repos)
+        self.assertEqual(config.list_repositories(), repo_names)
 
     def test_get_repository_config_default(self):
-        from pkgmeta.config import RepositoryConfig
-        # get_repository_config method test
-        self._init_config()
-        config = self._make_one(self.pkgmeta_cfg)
-        # Test without a repository name, should get the first listed repository.
+        repo_names = ['test1-repo', 'test2-repo']
+        repos = [self.make_repo_config(n) for n in repo_names]
+        config = self.make_one(repos)
+
+        # Test without a repository name,
+        # should return the default first listed repository.
         repo_config = config.get_repository_config()
+        from pkgmeta.config import RepositoryConfig
         self.assertTrue(isinstance(repo_config, RepositoryConfig))
-        self.assertEqual(repo_config.name, 'test1-repo')
+        self.assertEqual(repo_config.name, repo_names[0])
 
     def test_get_repository_config_with_name(self):
-        source_one = 'file:///home/repouser'
-        source_two = 'http://localhost:8020'
-        self._init_config(sources=dict(source_one=source_one, source_two=source_two))
-        config = self._make_one(self.pkgmeta_cfg)
-        # Second test with a specific repository name
-        repo_name = 'test2-repo'
+        repo_names = ['test1-repo', 'test2-repo', 'test3-repo', 'test4-repo']
+        repos = [self.make_repo_config(n) for n in repo_names]
+        # Create the config and specifically assign the default to
+        # a repo we aren't calling by name.
+        config = self.make_one(repos, default=repo_names[1])
+
+        # Test with a specific repository name
+        repo_name = repo_names[-1]
         repo_config = config.get_repository_config(repo_name)
-        self.assertEqual(repo_config.sources, [source_two, source_one])
-        repo_location = os.path.join(sysconfig.get_paths()['appdata.persistent'], repo_name)
-        self.assertEqual(repo_config.location, repo_location)
+        self.assertEqual(repo_config.name, repo_name)
